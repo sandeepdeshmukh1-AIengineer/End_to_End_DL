b74f4697586c5e7f5379b7812153228bd5f4f9d5
Sandeep Deshmukh <sandeepsdeshmukh1@gmail.com>
2025-03-12 15:20:22 +0530

    Initial commit - Deep Learning Project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-from cnnClassifier import logger
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
 
